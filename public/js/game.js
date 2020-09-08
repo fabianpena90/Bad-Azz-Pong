b2237ad25013a2d5c8cb629055d8a417c6aa8aaa
Fabian Pena <camilo.fabis@gmail.com>
2020-09-08 13:03:01 -0400

    Added score, interval variable, score in canvas

--- public/js/game.js
+++ public/js/game.js
@@ -6,72 +6,8 @@ const ctx = canvas.getContext("2d");
 const table = new Image()
 table.src = "/public/img/table.jpg"
 let id = null
-
-// Creating Ball
-let ballRadius = 10;
-let x = canvas.width / 2;
-let y = canvas.height - 30;
-let dx = 2;
-let dy = -2;
-
-// Funtion to draw ball
-function drawBall() { 
-  ctx.beginPath();
-  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-  ctx.fillStyle = "#fff";
-  ctx.fill();
-  ctx.closePath();
-}
-
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawBall();
-
-  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
-    dx = -dx;
-  }
-  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
-    dy = -dy;
-  }
-
-  let cooldown1 = false
-  let cooldown2 = false
-
-  if (racket1.x < x + ballRadius &&
-    racket1.x + racket1.w > x &&
-    racket1.y < y + ballRadius &&
-    racket1.y + racket1.h > y) {
-
-    //  non-functioning cooldown (always thinks cooldown1 is false)
-
-    //  if (cooldown1 == false){
-    //    console.log("cooldown activated")
-    //    cooldown1 = true
-    //    console.log(cooldown1)
-    //    dx = -dx;
-    //    setTimeout(function(){
-    //     console.log("cooldown ended")
-    //     cooldown1 = false
-    //   },3000)
-    //  } else {
-    //    console.log("cooldown1 is true")
-    //  }
-
-    dx = -dx
-  }
-
-  if (racket2.x < x + ballRadius &&
-    racket2.x + racket2.w > x &&
-    racket2.y < y + ballRadius &&
-    racket2.y + racket2.h > y) {
-    dx = -dx;
-  }
-
-  x += dx;
-  y += dy;
-}
-
-setInterval(draw, 10);
+let score1 = 0
+let score2 = 0
 
 // Rackets Class
 class Rackets {
@@ -92,36 +28,102 @@ class Rackets {
 let racket1 = new Rackets(10, 260, 20, 150)
 let racket2 = new Rackets(820, 260, 20, 150)
 
-let isHovering = false
+// Creating Ball
+let ballRadius = 10;
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let dx = 3;
+let dy = -3;
 
-canvas.addEventListener('mouseenter', e => {
-  isHovering = true
-});
+// Funtion to draw ball
+function drawBall() { 
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "#fff";
+  ctx.fill();
+  ctx.closePath();
+}
 
-canvas.addEventListener('mouseleave', e => {
-  isHovering = false
-});
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
 
-canvas.addEventListener('mousemove', e => {
-  if (isHovering === true) {
-    racket1.y = e.offsetY - (racket1.h/2)
+  if (x + dx > canvas.width - ballRadius) {
+    console.log("right")
+    score1++
+    clearInterval(interval)
+  } else if (x + dx < ballRadius) {
+    console.log("left")
+    score2++
+    clearInterval(interval)
   }
-});
+
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dy = -dy;
+  }
+
+  // Adding collision with Paddles
+  if (racket1.x < x + ballRadius &&
+    racket1.x + racket1.w > x &&
+    racket1.y < y + ballRadius &&
+    racket1.y + racket1.h > y) {
+    dx = -dx
+  }
+
+  // Adding collision with Paddles
+  if (racket2.x < x + ballRadius &&
+    //racket2.x + racket2.w > x &&
+    racket2.y < y + ballRadius &&
+    racket2.y + racket2.h > y) {
+    dx = -dx;
+  }
+
+  x += dx;
+  y += dy;
+}
+
+let interval = setInterval(draw, 10);
+
+function drawScore() {
+  ctx.font = "20px Arial";
+  ctx.fillStyle = "#fff";
+  ctx.fillText("Score: "+score1, 8, 20);
+  ctx.font = "20px Arial";
+  ctx.fillStyle = "#fff";
+  ctx.fillText("Score: "+score2, 750, 20);
+}
+
+
+// Mouse Paddles Control
+// let isHovering = false
+// canvas.addEventListener('mouseenter', e => {
+//   isHovering = true
+// });
+
+// canvas.addEventListener('mouseleave', e => {
+//   isHovering = false
+// });
+
+// canvas.addEventListener('mousemove', e => {
+//   if (isHovering === true) {
+//     racket1.y = e.offsetY - (racket1.h/2)
+//   }
+// });
 
 window.onkeydown = function (e) {
   console.log(e.key);
   switch (e.key) {
     case 'ArrowUp':
-      racket1.y -= 20
+      racket2.y -= 40
       break;
     case 'ArrowDown':
-      racket1.y += 20
+      racket2.y += 40
       break;
     case 'w':
-      racket2.y -= 20
+      racket1.y -= 40
       break;
     case 's':
-      racket2.y += 20
+      racket1.y += 40
       break;
   }
 
@@ -136,6 +138,7 @@ function animate() {
   racket1.drawRackets()
   racket2.drawRackets()
   drawBall();
+  drawScore()
 }
 
 animate()
