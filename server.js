7c0458f9f8f5dd765e46e33adf898cf55ab3e767
rabiulalam1 <rabiul.alam@outlook.com>
2020-09-08 15:32:30 -0400

    minor syntax error fixed

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + 'public'));
-
+app.use(express.static(__dirname + "public"));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/game", function(req, res) {
-  res.sendFile(__dirname + "/game.html")
-})
+app.get("/game", function (req, res) {
+  res.sendFile(__dirname + "/game.html");
+});
 
 // Listening when accessed by new user
 io.on("connection", (socket) => {
-  console.log('a user connected');
-  socket.on('message', (msg) => {
-    socket.broadcast.emit('message', msg)
+  console.log("a user connected");
+  socket.on("message", (msg) => {
+    socket.broadcast.emit("message", msg);
   });
 });
 
